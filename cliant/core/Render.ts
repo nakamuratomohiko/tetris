faa97381a694b29154316a090cdab3dcb210ab51
kbc14a08 <kbc14a08@stu.kawahara.ac.jp>
2016-07-08 13:29:25 +0900

    Rendrerクラス、変更
    キャンバスのサイズ のプロパティがあったが、
    ブロックを作ってそのサイズを個数でかけるように変更

--- cliant/core/Render.ts
+++ cliant/core/Render.ts
@@ -9,28 +9,29 @@ export class Render{
     //壁はblack,whiteはカラ,配列はnumberではなく色配列とする
     private canvas = document.getElementsByTagName('canvas')[0];
     private ctx = this.canvas.getContext('2d');
-    private w:number =400;//キャンパスのサイズ
-    private h:number = 800;//キャンパスのサイズ
+    private size = 40;
     private cols:number = 10;//横分割個数
     private rows:number = 20;//縦分割個数
-    private blockW = this.w / this.cols;//横を１０個のブロックに分ける
-    private blockH = this.h / this.rows;//縦を２０個のブロックにわける
     private result:Board[][];
     private block:Block;//操作しているBlock
 
-    constructor(result:Board[][],block:Block){
+    constructor(result:Board[][]){
         this.result = result;
-        this.block = block;
     }
 
+    public setBlock(block):void{
+        this.block = block;
+    }
+    
     /**
      * キャンバスに書き込む
      */
     public render(){
         //キャンバスをリセット
-        this.ctx.clearRect(0,0,this.w,this.h);
+        this.ctx.clearRect(0,0,this.size*this.cols,this.size * this.rows);
         this.ctx.strokeStyle = 'black';
         const blocks = this.block.form[this.block.angle];
+        const point = this.block.point;
 
         for(let x = 0; x < this.cols; x++){
             for(let y = 0; y < this.rows; y++){
@@ -44,8 +45,12 @@ export class Render{
 
             }
         }
+        
+        this.ctx.fillStyle = this.block.color;
+        this.drawBlock(this.block.point.x,this.block.point.y);
         for(let b in blocks){
-            this.drawBlock(blocks[b].x,blocks[b].y);
+            console.log("block");
+            this.drawBlock(blocks[b].x + point.x,blocks[b].y + point.y);
         }
     }
 
@@ -55,8 +60,8 @@ export class Render{
      * @param y {number}
      */
     private drawBlock(x:number,y:number){
-        this.ctx.fillRect(this.blockW * x, this.blockH * y,this.blockW,this.blockH);
-        this.ctx.strokeRect(this.blockW * x, this.blockH * y,this.blockW, this.blockH);
+        this.ctx.fillRect(this.size * x, this.size * y,this.size,this.size);
+        this.ctx.strokeRect(this.size * x, this.size * y,this.size, this.size);
     }
 
 }
