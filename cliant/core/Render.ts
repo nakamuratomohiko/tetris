c2248a53bfd1919978a683800bc15345097c1e66
kbc14a08 <kbc14a08@stu.kawahara.ac.jp>
2016-07-11 22:00:45 +0900

    Renderクラス、変更
    ブロックがない時の処理追加

--- cliant/core/Render.ts
+++ cliant/core/Render.ts
@@ -9,7 +9,7 @@ export class Render{
     //壁はblack,whiteはカラ,配列はnumberではなく色配列とする
     private canvas = document.getElementsByTagName('canvas')[0];
     private ctx = this.canvas.getContext('2d');
-    private size = 40;
+    private size = 30;
     private cols:number = 10;//横分割個数
     private rows:number = 20;//縦分割個数
     private result:Board[][];
@@ -17,6 +17,7 @@ export class Render{
 
     constructor(result:Board[][]){
         this.result = result;
+        this.render();
     }
 
     public setBlock(block):void{
@@ -30,8 +31,6 @@ export class Render{
         //キャンバスをリセット
         this.ctx.clearRect(0,0,this.size*this.cols,this.size * this.rows);
         this.ctx.strokeStyle = 'black';
-        const blocks = this.block.form[this.block.angle];
-        const point = this.block.point;
 
         for(let x = 0; x < this.cols; x++){
             for(let y = 0; y < this.rows; y++){
@@ -45,11 +44,15 @@ export class Render{
 
             }
         }
-        
-        this.ctx.fillStyle = this.block.color;
-        this.drawBlock(this.block.point.x,this.block.point.y);
-        for(let b in blocks){
-            this.drawBlock(blocks[b].x + point.x,blocks[b].y + point.y);
+
+        if(this.block !== undefined) {
+            const blocks = this.block.form[this.block.angle];
+            const point = this.block.point;
+            this.ctx.fillStyle = this.block.color;
+            this.drawBlock(this.block.point.x, this.block.point.y);
+            for (let b in blocks) {
+                this.drawBlock(blocks[b].x + point.x, blocks[b].y + point.y);
+            }
         }
     }
 
