f7cc96d06eb3c2ae639bb255d694fa3fdc25a9ac
kbc14a08 <kbc14a08@stu.kawahara.ac.jp>
2016-07-12 11:04:41 +0900

    TetrisContollerクラス　変更
    コンストラクタ内 
    　　　readyメソッド呼び出し、ボタンstart,refreshにメソッドを追加
    メソッドready変更　
    　　　　戻り値なし
    　　　　html要素の空白文字にする
    　　　　communicatorのreadyメソッドをつかう
    setUserメソッド削除
    setBlockListメソッド追加
    　　　引数　BlockType[]
    　　　戻り値なし
              テトリスにセットする
    notifyScoreメソッドコメント追加
    getScoreメソッド　削除
    Errorメソッド　実装
    　　　コメント追加
    　　　html id=errorにメッセージを入れる
        　　ゲームを終了させる
    finishGameメソッド　コメント追加　変更
    　　　html要素　nameから名前を取得して送る
    onkeydownで画面全体が動かないように
    e.preventDefaultを追加
    
    　　　　
    　

--- cliant/core/TetrisController.ts
+++ cliant/core/TetrisController.ts
@@ -18,15 +18,37 @@ export class TetrisController{
         this.tetris = new Tetris(this);
         document.body.onkeydown = (e)=>this.onKeyDown(e);
         this.user = new User();
+        this.ready();
+        document.getElementById("refresh").onclick = ()=> this.ready();
+        document.getElementById("start").onclick = ()=>this.start();
+
     }
 
     /**
      * ClientのTetrisのセット
-     * @param blockList {BlockType}
      */
-    public ready(blockList:BlockType[]){
-        this.tetris.setBlockList(blockList);
+    public ready(){
+        //初期化する
+        const s = <HTMLInputElement>document.getElementById("score");
+        s.innerHTML = "";
+        const e = <HTMLInputElement>document.getElementById("error");
+        e.innerHTML = "";
+        const a = <HTMLInputElement>document.getElementById("start");
+        a.disabled = true;
+        this.commu.ready();
+
     }
+
+    /**
+     * BlockType[]をTetrisクラスにセットする
+     * @param blockList
+     */
+    public setBlockList(blockList:BlockType[]){
+        this.tetris.setBlockList(blockList);
+        const a = <HTMLInputElement>document.getElementById("start");
+        a.disabled = false;
+    }
+
 
     /**
      * ClientのTetrisを動かす
@@ -35,26 +57,13 @@ export class TetrisController{
         this.tetris.newGame();
     }
 
-    /**
-     * ユーザをセットする
-     * @param user
+      /**
+     * スコアを通知する
+     * @param score
      */
-    public setUser(name:string,score:number){
-        this.user.name = name;
-        this.user.score = score;
-    }
-
-    /**
-     * ユーザのスコアをセットする
-     * @returns {number}
-     */
-    public getScore():number{
-        return this.user.score;
-    }
-
-    //ユーザのスコアよりも高かった場合に表示、ユーザのスコア更新
     public notifyScore(score:number){
-
+        let p = document.getElementById("score");
+        p.innerHTML = score+"POINT";
     }
 
     /**
@@ -66,19 +75,25 @@ export class TetrisController{
         this.commu.pushBlock(block);
         
     }
-        
-    //エラーが起きたときに表示する
-    public Error(msg:string){
 
+    /**
+     * 不正な操作のときにたたかれる
+     * @param msg
+     */
+    public Error(msg){
+        let p = document.getElementById("error");
+        p.innerHTML = msg;
+        this.tetris.invalidOperation();
     }
     
     
 
-    
-
-
+    /**
+     * 先に入れていた名前をサーバに送ってスコアを登録する
+     */
     public finishGame():void{
-        //serverに終了を告げる
+        const name:HTMLInputElement = <HTMLInputElement>document.getElementById("name");
+        this.commu.finishGame(name.value);
     }
 
     /**
@@ -97,6 +112,7 @@ export class TetrisController{
 
         if(typeof keys[e.keyCode] != 'undefined'){
             if(!this.tetris.lose) {
+                e.preventDefault();
                 this.keyDown(keys[e.keyCode]);
             }
         }
