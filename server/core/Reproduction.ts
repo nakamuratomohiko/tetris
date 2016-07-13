d06ac5982fa80efade32e993d93a0c28c72564a4
kbc14a08 <kbc14a08@stu.kawahara.ac.jp>
2016-07-13 11:07:06 +0900

    Reproductionクラス
    変更
    　result number[][] -> Board[][]
    　ready メソッド　コメント
    　verificationメソッド　コメント　
        メソッドverification
    　　public -> private
    　　処理もserverが作成したBlockインスタンスをしようする
    　　仮数字-1から-2に変更
    　　changeメソッド　処理
    　　-1 -> -2を見つけるようにする
      　 calculationメソッドの処理で配列のtypeを見るように
    　　initメソッド  　result配列の中身をnumber からBordar型に変更
     　clearLineメソッドをpublic からprivte
    追加
    　プロパティ
    　　cols : number 
    　　rows:number 
           blockFactory:BlockFactory
    　メソッド:finish
    　　引数追加
    　pushBlockメソッド　
    　
    削除
    　プロパティ　user

--- server/core/Reproduction.ts
+++ server/core/Reproduction.ts
@@ -4,55 +4,91 @@ import {User} from "../model/User";
 import {BlockType} from "../model/BlockType";
 import Promise = require("any-promise/index");
 import {DBStore} from "../db/DBStore";
+import {BlockFactory} from "../model/BlockFactory";
+import {Board} from "../model/Board";
+import {Generator} from "./Generator";
 /**
  * Created by vista on 2016/07/05.
  */
 
 
-export class Reproduction{
+export class Reproduction {
 
     private _score:number;
-    private result:number[][];
+    private result:Board[][];
     private blockList:BlockType[];
-    private user:User;
     private _startTime:Date;
     private dbStore:DBStore;
-    private _endTime:Date ;
+    private _endTime:Date;
+    private blockFactry:BlockFactory;
+    private cols:number  = 10;
+    private rows:number = 20;
+    private genrator:Generator;
 
-    constructor(user:User){
-        this.user = user;
+    constructor(){
         this.dbStore = new DBStore();
+        this.blockFactry = BlockFactory.getInstance();
+        this.genrator = Generator.getInstance();
     }
 
     public set startTime(startTime:Date){
         this._startTime = startTime;
     }
+
     public get startTime():Date{
         return this._startTime;
     }
+
     public get endTime():Date{
         return this._endTime;
     }
+
     public set endTime(endTime){
         this._endTime = endTime;
     }
+
     public get score():number{
         return this._score;
     }
 
-
-
     /**
-     * ゲームの終了でデータベースにデータを入れる、最高得点のときのみ
+     * 準備、リセット
+     */
+    public ready():BlockType[]{
+        this.init();
+        this.blockList = this.genrator.createList();
+        return this.blockList;
+    }
+    
+    /**
+     * ゲームの終了でデータベースにデータを入れる
      * @returns {Promise}
      */
-    public finish():Promise<any>{
-        return new Promise(function (resolve,reject) {
-            this.dbStore.scoreUpdate(this.user)
-                .then(res =>{resolve()})
-                .catch(err =>{reject()});
-                        
+    public finish(name:string,score:number):Promise<void> {
+        return new Promise(function (resolve, reject) {
+            this.dbStore.scoreInsert(name,score)
+                .then(res => { resolve();})
+                .catch(err => {reject();});
+
         });
+    }
+
+    /**
+     * ブロックを送られてくる、検証から計算、ラインの削除まで行う
+     * @param block
+     * @returns {boolean}
+     */
+    public pushBlock(block:Block):boolean{
+        if( this.verification(block) ){
+            this.change(1);
+            this.calculation();
+            this.clearLine();
+            return true;
+        }else{
+            this.change(0);
+            return false;
+        }
+
     }
 
     /**
@@ -60,56 +96,53 @@ export class Reproduction{
      * @param block
      * @returns {boolean}
      */
-    public verification(block:Block):boolean{
+    private verification(block:Block):boolean {
 
         //置いた時間が終了時間を超えていた場合不正とみなす。
-        if(this.endTime < block.date){return false;}
+        if (this.endTime < block.date) {return false;}
 
-        this.blockList.pop();//ここで引数のブロックのタイプから手元で同じものを作成してテストを始める
-        const px:number = block.point.x;
-        const py:number = block.point.y;
-        let form:Point[][] = block.form;
-        //ここで検証する
-        if(this.result[px][py] == 1){
-            //失敗
-            this.change(0);
-            return false;
-
-        }else{//成功
-            for(let i of form[block.angle]){
+        //配列から引っ張ってきて、引数と比較
+        if(this.blockList.pop() == block.blockType){
+            const px:number = block.point.x;
+            const py:number = block.point.y;
+            const tBlock = this.blockFactry.getBlock(block.blockType);
+            if(tBlock === undefined){return false}
+            //ここで検証する
+            if (this.result[px][py].type == 1 || this.result[px][py].type == -1) { return false;}
+            for (let i of tBlock.form[block.angle]) {
                 let x = i.x;
                 let y = i.y;
 
-                if(this.result[block.point.x+x][block.point.y+y] == 1){
+                if (this.result[px + x][py + y].type == 1 ||
+                        this.result[px + x][py + y].type == -1) {
                     //失敗
-                    this.change(0);
                     return false;
-                }else {
+                } else {
                     //仮で入れる
-                    this.result[block.point.x+x][block.point.y+y] = -1;
+                    this.result[px + x][py + y].type = -2;
                 }
 
 
             }
 
-        }
-        this.change(1);
-        this.calculation();
-        return true;
+            return true;    
+        }//ここで引数のブロックのタイプから手元で同じものを作成してテストを始める
+        
     }
 
     /**
      * 仮で入っている-1を引数の数字に変換する
      * @param num {number}
      */
-    private change(num:number){
-        for(let i:number = 0; i < this.result[0].length;i++){
-            for(let j = 0; this.result.length;j++){
-                if(this.result[j][i] == -1){this.result[j][i] = num}
+    private change(num:number) {
+        for (let i:number = 0; i < this.result[0].length; i++) {
+            for (let j = 0; this.result.length; j++) {
+                if (this.result[j][i].type == -2) {
+                    this.result[j][i].type = num
+                }
             }
         }
     }
-
 
 
     /**
@@ -125,26 +158,66 @@ export class Reproduction{
             //左から右に
             for(let j:number = 0; j < this.result.length;j++){
 
-                if(this.result[j][i]==1 ){
+                if(this.result[j][i].type==1){
                     count += 1;
                 }
-                if(count == this.result.length){
-                    lane += 1;
-                    //一直線に埋まっているlaneは0を入れる
-                    for( let c:number = 1; c < this.result.length -1; c++){
-                        this.result[c][i] = 0;
+            }
+            if (count == this.result.length) {
+                lane += 1;
+            }
+
+        }
+
+        this.score = this.score + (lane * 10);
+    }
+
+    /**
+     * result配列を初期化する
+     */
+    private init() {
+        for (let x = 0; x < this.cols; x++) {
+            for (let y = 0; y < this.rows; y++) {
+                if (x == 0 || x == this.cols - 1) {
+                    this.result[x][y] = {type: -1, color: ""};
+
+                } else if (y == (this.rows - 1) || y == 0) {
+                    this.result[x][y] = {type: -1, color: ""};
+                    
+                } else {
+                    this.result[x][y] = {type: 0, color: ""};
+                }
+
+            }
+        }
+
+    }
+    
+    /**
+     * 一列に並んでいるラインがあれば消す
+     */
+    private
+    clearLine() {
+
+        for (let y = 5; y < this.rows -1 ; y++) {
+            let rowLine = true;
+            for (let x = 0; x < this.cols ; x++) {
+                if (this.result[x][y].type == 0) {
+                    rowLine = false;
+                    break;
+                }
+            }
+
+            if (rowLine) {
+                for (let yy = y; yy > 5; yy--) {
+                    for (let x = 1; x < this.cols - 1; x++) {
+                        this.result[x][yy] = this.result[x][yy - 1];
                     }
                 }
 
             }
 
         }
-
-        this.score = this.score + (lane * 100);
     }
-
-
-
 
 
 }
